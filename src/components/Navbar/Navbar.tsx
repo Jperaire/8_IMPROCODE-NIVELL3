75338e79ba6f27860c94d88feb7a96fde977503b
Julen <julenperaire@gmail.com>
2025-07-24 11:48:02 +0200

    feat: instalacion de firebase

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,39 +3,49 @@ import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
     return (
-        <nav className={styles.navbar}>
-            <NavLink
-                to="/"
-                className={({ isActive }) =>
-                    isActive ? `${styles.link} ${styles.active}` : styles.link
-                }
-            >
-                Home
-            </NavLink>
-            <NavLink
-                to="/calendar"
-                className={({ isActive }) =>
-                    isActive ? `${styles.link} ${styles.active}` : styles.link
-                }
-            >
-                Calendar
-            </NavLink>
-            <NavLink
-                to="/grafics"
-                className={({ isActive }) =>
-                    isActive ? `${styles.link} ${styles.active}` : styles.link
-                }
-            >
-                Grafics
-            </NavLink>
-            <NavLink
-                to="/map"
-                className={({ isActive }) =>
-                    isActive ? `${styles.link} ${styles.active}` : styles.link
-                }
-            >
-                Map
-            </NavLink>
+        <nav className={`${styles.navbar} mb-3`}>
+            <div className="container d-flex gap-4">
+                <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                        isActive
+                            ? `${styles.link} ${styles.active}`
+                            : styles.link
+                    }
+                >
+                    Home
+                </NavLink>
+                <NavLink
+                    to="/calendar"
+                    className={({ isActive }) =>
+                        isActive
+                            ? `${styles.link} ${styles.active}`
+                            : styles.link
+                    }
+                >
+                    Calendar
+                </NavLink>
+                <NavLink
+                    to="/grafics"
+                    className={({ isActive }) =>
+                        isActive
+                            ? `${styles.link} ${styles.active}`
+                            : styles.link
+                    }
+                >
+                    Grafics
+                </NavLink>
+                <NavLink
+                    to="/map"
+                    className={({ isActive }) =>
+                        isActive
+                            ? `${styles.link} ${styles.active}`
+                            : styles.link
+                    }
+                >
+                    Map
+                </NavLink>
+            </div>
         </nav>
     );
 };
--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,3 +1,49 @@
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../../firebase/firestore";
+
+interface Ubicacions {
+    id: string;
+    categoria: string;
+    lat: number;
+    lng: number;
+    nom: string;
+}
+
 export const MapPage = () => {
-    return <>MapPage</>;
+    const [ubicacions, setUbicacions] = useState<Ubicacions[]>([]);
+
+    useEffect(() => {
+        const fetchUbicacions = async () => {
+            const dataImported = await getDocs(collection(db, "lugares"));
+            const data = dataImported.docs.map((doc) => ({
+                id: doc.id,
+                categoria: doc.data().categoria,
+                lat: doc.data().lat,
+                lng: doc.data().lng,
+                nom: doc.data().nom,
+            }));
+            console.log(data);
+            setUbicacions(data);
+        };
+
+        fetchUbicacions();
+    }, []);
+
+    return (
+        <>
+            <h1>MapPage</h1>
+            <div className="p-4">
+                <h2 className="text-xl font-bold mb-2">Llistat d'Ubicacions</h2>
+                <ul className="space-y-2">
+                    {ubicacions.map((u) => (
+                        <li key={u.id} className="border p-2 rounded shadow">
+                            <strong>{u.nom}</strong>
+                            Edad: {u.id}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    );
 };
